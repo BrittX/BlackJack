3c78bdfa2857e9890eb5021d18ef227da040e293
Brittany Barnes <Brittany Barnes>
2016-08-06 13:01:18 -0700

    Hand and Deck classes added

--- BlackJack.py
+++ BlackJack.py
@@ -31,20 +31,78 @@ class Card(object):
 	suit_name = ("Clubs", "Spades", "Diamonds", "Hearts")
 	rank_name = ("Ace", "2", "3", "4", "5", "6", "7", "8", "9",
 						"10", "Jack", "Queen", "King")
+	card_val = {"Ace": 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6,
+						'7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10,
+							'Queen': 10, 'King': 10}
 						
-	def __init__(self, rank, suit):
+	def __init__(self, rank, suits):
 		self.rank = rank
-		self.suit = suit
+		self.suits = suits
 		
 	def __str__(self):
-		return self.suit + self.rank
+		return self.suits + self.rank
 		
 	def get_rank(self):
 		return self.rank
 	
 	def get_suit(self):
-		return self.suit
+		return self.suits
 		
 	def draw(self):
-		print(self.suit + self.rank)
+		print(self.suits + self.rank)
 		
+"""
+Deck class 
+"""
+class Deck(object):
+
+	# Initialize a deck of all the cards
+	def __init__(self):
+		self.deck = []
+		for suit in suits:
+			for rank in ranking
+				self.deck.append(Card(suits, rank)
+				
+	# Method to shuffle deck
+	def shuffle(self):
+		random.shuffle(self.deck)
+	
+	# Deal out one card at a time
+	def deal(self):
+		dealt_card = self.deck.pop()
+		return dealt_card
+		
+class Hand(object):
+	def __init__(self):
+		self.cards = []
+		self.value = 0
+		self.ace = False
+		
+	def __str__(self):
+		curr_hand = ""
+		
+		for card in self.cards:
+			name = card.__str__()
+			curr_hand += " " + name
+			
+		return 'You currently have: %s' %curr_hand
+		
+	# Add a card to your hand
+	def add_card(self, card):
+		self.cards.append(card)
+		
+		# Check if one of the cards is an Ace
+		if card.rank == 'Ace':
+			self.ace = True
+			
+		self.value += card_val[card.rank]
+		
+	# Return the value of your hand
+	def card_value(self):
+		# Have an ace and total hand is less than 12
+		if(self.ace == True and self.value < 12):
+			return self.value + 10
+		else:
+			return self.value
+				
+			
